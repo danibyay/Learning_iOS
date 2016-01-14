181aaec4b86b2b1121b896ae27a9982a990a2964
Daniela Becerra <madajiji@gmail.com>
2016-01-14 09:25:01 -0600

    add initial files of an xcode project

--- test-ios-itesm/ViewController.swift
+++ test-ios-itesm/ViewController.swift
@@ -10,14 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dbgLabel: UILabel!
+    
+    var toggle = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let name: String = "Daniela"
+        let age: Int = 20
+        
+        printDanielaCharacteristics(name, age: age)
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func printDanielaCharacteristics(name: String, age: Int){
+        print(name)
+        print(age)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonWasPressed(sender: AnyObject) {
+        if(toggle){
+            dbgLabel.text = "WOW it changed"
+            toggle = !toggle
+        }
+        else{
+            dbgLabel.text = "Wow it's different"
+            toggle = !toggle
+        }
+        
+       
     }
 
 
