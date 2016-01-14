0f3595616a1e806057b0e844be933afddeb2efb0
Daniela Becerra <madajiji@gmail.com>
2016-01-14 09:45:16 -0600

    the swipe gesture recognizer works when swiped to the right

--- test-ios-itesm/ViewController.swift
+++ test-ios-itesm/ViewController.swift
@@ -21,15 +21,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func swipeWasFound(sender: UISwipeGestureRecognizer) {
-        print("swipe was found")
-    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func swipeWasReceived(sender: UISwipeGestureRecognizer) {
+        dbgLabel.text = "swiped right"
+    }
     @IBAction func buttonWasPressed(sender: AnyObject) {
         if(toggle){
             dbgLabel.text = "WOW it changed"
